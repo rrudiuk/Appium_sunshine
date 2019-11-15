420f4b26cff0dd78a3a1a444b233109f07b228a2
rrudiuk <rus.barik@gmail.com>
2019-11-15 17:44:10 +0200

    BasePO class created

---
+++ src/test/java/pageobjects/BasePO.java
@@ -0,0 +1,45 @@
+package pageobjects;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+import utils.PropertyUtils;
+import utils.WaitUtils;
+
+import java.time.Duration;
+
+public class BasePO {
+
+    public final static int IMPLICIT_WAIT = PropertyUtils.getIntegerProperty("implicitWait", 30);
+    WaitUtils waitUtils;
+    protected final AppiumDriver driver;
+
+    /**
+     * A base constructor that sets the page's driver.
+     *
+     * The page structure is being used within this test in order to separate
+     * the page actions from the tests.
+     *
+     * Please use the AppiumFieldDecorator class within the page factory. This way annotations
+     * like @AndroidFindBy within the page objects.
+     *
+     * @param driver the Appium driver created in the beforesuite method.
+     */
+
+    protected BasePO(AppiumDriver driver) {
+        this.driver = driver;
+        initElements();
+        loadProperties();
+        waitUtils = new WaitUtils();
+    }
+
+    // Initialize the web elements
+    private void initElements() {
+        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(IMPLICIT_WAIT)), this);
+    }
+
+    private void  loadProperties() {
+        // TODO: Add the properties
+    }
+
+}
